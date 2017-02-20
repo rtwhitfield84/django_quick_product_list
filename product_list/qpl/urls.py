38cd4dfa3f06d58481bd0933520901d49dbacd16
Rico <richardthomaswhitfield@gmail.com>
2017-02-20 15:32:12 -0600

    setting up auth

--- product_list/qpl/urls.py
+++ product_list/qpl/urls.py
@@ -4,7 +4,11 @@ from qpl import views
 app_name = 'qpl'
 
 urlpatterns = [
-	url(r'^$', views.ProductsView.as_view(), name='products'),
+	url(r'^$', views.BaseView.as_view(), name='base'),
+	url(r'^login', views.login_user, name='login'),
+	url(r'^register', views.RegisterView.as_view(), name='register'),
+	url(r'^register', views.register_user, name='register_user'),
+	url(r'^products', views.ProductsView.as_view(), name='products'),
 	url(r'^product_added', views.ProductAddedView.as_view(), name='product_added'),
 
 ]
--- product_list/qpl/views.py
+++ product_list/qpl/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.views import View
-from django .http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth import logout, login, authenticate
+from django .http import HttpResponse, HttpResponseRedirect
 from . models import Product
+
+
+class BaseView(TemplateView):
+	template_name = 'base.html'
+
+class LoginView(TemplateView):
+	template_name = 'login.html'
+
+
+class RegisterView(TemplateView):
+	template_name = 'register.html'
 
 
 class ProductsView(View):
@@ -26,3 +39,35 @@ class ProductsView(View):
 
 class ProductAddedView(TemplateView):
 	template_name = 'productAdded.html'
+
+def register_user(request):
+	data = request.POST
+	User.objects.create_user(
+		username=data['username'],
+		email=data['email'],
+		first_name=data['first_name'],
+		last_name=data['last_name'],
+		password=data['password']
+	)
+
+	return LoginView.login_user(request)
+
+def login_user(request):
+    data = request.POST
+    username = data['username']
+    password = data['password']
+    user = authenticate(
+    	username=username,
+    	password=password
+    )
+
+    if user is not None:
+    	login(request=request, user=user)
+    else:
+    	HttpResponseRedirect(redirect_to='/register')
+    return HttpResponseRedirect(redirect_to='/products')
+
+
+def logout_user(request):
+	logout(request)
+	return HttpResponseRedirect(redirect_to='/')
